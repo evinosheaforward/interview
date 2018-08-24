a759e54cb424ff7df8313106a5085154116f510a
Evin O'Shea <eco2000@wildcats.unh.edu>
2018-08-23 23:01:42 -0400

    Added an outline for main.go and some important files

---
+++ src/app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+  "io"
+  //filestat
+)
+
+//main function for running fileStat process
+//have either fileHandler or report come through channel
+func main() {
+  x =: make(chan io.Reader)
+  y =: make(chan io.Writer)
+
+  go func() { r <- FileStreamer() }
+  go func() { w <- NewsManager()}
+  while true:
+    // I want FileStreamer and NewsManager to run concurrently
+    select {
+    case file <- r:
+      // run the ingest code for the file
+      go Ingest(file)
+      fmt.PrintLn("Ingesting file: %s", file)
+    case reporter <- w:
+      err = reporter.Report()
+      if err {
+        fmt.PrintLn("Uh oh!")
+      }
+      break
+    }
+  fmt.PrintLn("Done")
+}
